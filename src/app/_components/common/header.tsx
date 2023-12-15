a63ef96103b26c1fd586dda983798c0e24093631
Takumi Maruyama <satouredstone@gmail.com>
2023-12-16 01:06:39 +0900

    色々消した

---
+++ src/app/_components/common/header.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto flex items-center justify-between">
+        <h1 className="text-2xl font-bold">クソザコエンジニア奮闘記</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
