e713212f319da4d96c647da8e96d3282fd8e4b85
Takumi Maruyama <satouredstone@gmail.com>
2023-12-16 00:54:30 +0900

    contentfulを剥がす

--- utils/contentful.ts
+++
@@ -1,7 +0,0 @@
-import { CreateClientParams, createClient } from "contentful";
-const config: CreateClientParams = {
-  space: process.env.NEXT_PUBLIC_CTF_SPACE_ID!,
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_DELIVERY_TOKEN!,
-};
-
-export const client = createClient(config);
