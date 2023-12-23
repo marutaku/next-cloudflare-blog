c84ce14c4b33594da2a9db87962922945be513a0
Takumi Maruyama <satouredstone@gmail.com>
2023-12-23 23:16:04 +0900

    UI調整

--- src/app/_components/common/footer.tsx
+++ src/app/_components/common/footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="footer bg-gray-800 text-white py-4 relative w-screen bottom-0">
       <div className="container mx-auto text-right flex items-center justify-end">
-        <p>© {format(new Date(), "yyyy")} クソザコエンジニア奮闘記</p>
+        <p className="p-2">© {format(new Date(), "yyyy")} クソザコエンジニア奮闘記</p>
       </div>
     </footer>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
 
   return (
     <main className="py-12 container mx-auto">
-      <h1 className="text-3xl font-bold mb-8">記事一覧</h1>
+      <h1 className="text-3xl font-bold mx-2 my-4">記事一覧</h1>
       <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-4  mx-auto">
         {articleMetadata.map((article) => (
           <div className="px-2" key={article.slug}>
