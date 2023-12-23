1a4288fbd5b72b85cafcdfacac1e5038b92bdd3b
Takumi Maruyama <satouredstone@gmail.com>
2023-12-24 00:13:39 +0900

    footer修正

--- src/app/_components/common/footer.tsx
+++ src/app/_components/common/footer.tsx
@@ -3,7 +3,7 @@ import { format } from "date-fns";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 relative w-screen bottom-0 flex items-center justify-end">
+    <footer className="footer bg-gray-800 relative w-screen bottom-0 flex items-center justify-end">
       <p className="container mx-auto px-4 py-2 text-white text-right">
         © {format(new Date(), "yyyy")} クソザコエンジニア奮闘記
       </p>
