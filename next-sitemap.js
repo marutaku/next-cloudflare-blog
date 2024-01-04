80689b221cecc02f46d44cd1056e95040b5ecc85
Takumi Maruyama <satouredstone@gmail.com>
2024-01-04 23:42:51 +0900

    Fix siteUrl typo in next-sitemap.js

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://blog.marrutaku.info",
+  siteUrl: "https://blog.marutaku.info",
   generateRobotsTxt: true,
   outDir: "./out",
 };
