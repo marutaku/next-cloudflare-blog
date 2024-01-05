83d47008d1d4150172df17d91e8700a91b476a07
Takumi Maruyama <satouredstone@gmail.com>
2024-01-05 22:34:36 +0900

    変えてみた

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,7 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "blog.marutaku.info",
+  siteUrl: "https://blog.marutaku.info/",
   generateRobotsTxt: true,
   outDir: "./out",
   sitemapSize: 7000,
+  generateIndexSitemap: false,
 };
