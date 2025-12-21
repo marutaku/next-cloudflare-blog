8d135b985b2ca7212e3df4af37d804e5289309b0
Takumi Maruyama <satouredstone@gmail.com>
2025-12-21 15:27:10 +0900

    v4へのマイグレーション

--- src/utils/apollo-client.ts
+++ src/utils/apollo-client.ts
@@ -1,4 +1,12 @@
-import { ApolloClient, InMemoryCache, from, HttpLink } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloLink,
+} from "@apollo/client";
+
+import { Defer20220824Handler } from "@apollo/client/incremental";
+import { LocalState } from "@apollo/client/local-state";
 
 const TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN;
 const SPACE = process.env.CONTENTFUL_SPACE_ID;
@@ -11,13 +19,43 @@ const http = new HttpLink({
   },
 });
 
-const link = from([http]);
+const link = ApolloLink.from([http]);
 
 const cache = new InMemoryCache();
 
 const apolloClient = new ApolloClient({
   link,
   cache,
+
+  /*
+  Inserted by Apollo Client 3->4 migration codemod.
+  If you are not using the `@client` directive in your application,
+  you can safely remove this option.
+  */
+  localState: new LocalState({}),
+
+  /*
+  Inserted by Apollo Client 3->4 migration codemod.
+  If you are not using the `@defer` directive in your application,
+  you can safely remove this option.
+  */
+  incrementalHandler: new Defer20220824Handler(),
 });
 
 export default apolloClient;
+
+/*
+Start: Inserted by Apollo Client 3->4 migration codemod.
+Copy the contents of this block into a `.d.ts` file in your project to enable correct response types in your custom links.
+If you do not use the `@defer` directive in your application, you can safely remove this block.
+*/
+
+import "@apollo/client";
+
+declare module "@apollo/client" {
+  export interface TypeOverrides extends Defer20220824Handler.TypeOverrides {}
+}
+
+/*
+End: Inserted by Apollo Client 3->4 migration codemod.
+*/
